51d8bcf25a52d180cae690ec5141927d01cb2df0
EzyCzy <82717285+EzyCzy@users.noreply.github.com>
2022-02-01 16:19:58 +0800

    1.1v
    Added description

--- Competitive Programming/CSES/NumberSpiral.cpp
+++ Competitive Programming/CSES/NumberSpiral.cpp
@@ -5,7 +5,12 @@ typedef int64_t tt;
 typedef unsigned long long ll;
 using namespace std;
 /*
-Further Notice
+Let X = maximum value of x and y
+As brute force that is iterating through the entire 2D array, at worse, it will take O(t*X^2) equivalent to 10^9 * 10^9 * 10^5 = 10^23
+which is beyond the time we can afford. If we look carefully, every odd number of column and even number of row lies the square of 
+the array size at the end top or left of the array. Using that information, we can find X of a coordinate, we now know the coordinate lies in the range of (X-1)^2 + 1 and X^2.
+We can check for the value of x or y that is equals to X ( y>x works too) then decided our operation to either add to (X-1)^2 or subtract from X^2
+Using this method, for every case we get O(1) which result in O(t) for the entire test cases.
 */
 int main()
 {
