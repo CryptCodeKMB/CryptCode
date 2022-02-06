a817e8af249e7f91643823d47a5ec36d80ac63b4
EzyCzy <82717285+EzyCzy@users.noreply.github.com>
2022-02-06 20:27:32 +0800

    1.0v Updated with explanation

--- Competitive Programming/CSES/Introductory/TrailingZero.cpp
+++ Competitive Programming/CSES/Introductory/TrailingZero.cpp
@@ -12,8 +12,17 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 /*
 This one requires some mathematical reasoning. 
-Let n = 37. 
-
+Let n = 10. As you may know, to have zeros at the end
+of a number, you will see the divisor of 2 and 5 
+2*5 = 10, this is the case that causes the number to
+have trailing zeros at the end of the number.
+for our case of n, 
+1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10
+    ^       ^   ^   ^       ^       ^
+Let x be the number of divisor of 2 and y be divisor of 10
+we can see that x = 5, y = 2. From here we can conclude that
+x > y is always true. To find the number of zeros a number have
+we just need to count how many times we can divide the number by 5.
 */
 int n;
 
