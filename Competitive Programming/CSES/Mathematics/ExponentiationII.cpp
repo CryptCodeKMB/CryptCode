eedbf2f04313fd62f68c010a88d950ad07611563
EzyCzy <82717285+EzyCzy@users.noreply.github.com>
2022-02-06 11:53:05 +0800

    t

---
+++ Competitive Programming/CSES/Mathematics/ExponentiationII.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+typedef int64_t tt;
+typedef long long ll;
+using namespace std;
+
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+/*
+We utilize the technique in Exponentiation I, but a little twist
+according to Fermat's Little Theorem, $a^{p - 1} \equiv 1 \pmod{p}$
+, so we can calculate 
+$a^{b^c \pmod{1e9 + 7 - 1}} \pmod{1e9+7}$
+ with modular exponentiation.
+*/
+
+int fastExpo(int a, int n, int MOD){
+    int ans = 1;
+
+    while(n>=1){
+        if(n%2 == 0){
+            a = (1LL*a*a) % MOD;
+            n/=2;
+        } else {
+        ans = (1LL*ans*a) % MOD;
+        n--;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int n;
+    cin >> n;
+    const int MOD = 1e9+7;
+    for(int i = 0; i<n ;i++){
+        int a,b,c;
+        cin >> a >> b >> c;
+        cout << fastExpo(a,fastExpo(b,c, MOD-1), MOD) << "\n";
+    }
+    return 0;
+}
